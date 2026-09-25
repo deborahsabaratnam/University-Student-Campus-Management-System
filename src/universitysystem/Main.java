e67baa5b85b75422be96706da31e055952203b33
deborahsabaratnam <sabaratnamdeborah@gmail.com>
2026-09-25 09:27:05 +0530

    Create initial Java project structure

---
+++ src/universitysystem/Main.java
@@ -0,0 +1,8 @@
+package universitysystem;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("University Student and Campus Management System");
+    }
+}
